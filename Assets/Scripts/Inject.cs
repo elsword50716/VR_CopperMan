0d94241b9bccd47c1c626da299fa1315604abf50
DESKTOP-TJ048MB\elsword <c0970305160@gmail.com>
2020-07-08 22:40:57 +0800

    初版

---
+++ Assets/Scripts/Inject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inject : MonoBehaviour
+{
+    public GameObject niddle;
+    public float depth = 0.008123F;
+    public bool isInside = false;
+    Vector3 originalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+        niddle = GameObject.Find("Niddle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Acupunctures")
+        {
+            Debug.Log("Inject");
+            if(isInside)
+                transform.Translate(depth * Vector3.down);
+            niddle.GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = collision.transform;
+        }
+        
+        if(collision.gameObject.tag == "WrongPoint")
+        {
+            Debug.Log("reset pos");
+            gameObject.transform.position = originalPos;
+            niddle.GetComponent<Rigidbody>().isKinematic = false;
+            //Destroy(niddle.GetComponent<Rigidbody>());
+            transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/StickOn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool hit = false;
+    float depth = 0.30F;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!hit)
+        {
+            ArrowStick(other);
+            hit = true;
+        }
+    }
+    void ArrowStick(Collision2D col)
+    {
+
+        // move the arrow deep inside the enemy or whatever it sticks to
+        col.transform.Translate(depth * -Vector2.right);
+        // Make the arrow a child of the thing it's stuck to
+        transform.parent = col.transform;
+        //Destroy the arrow's rigidbody2D and collider2D
+        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(GetComponent<Collider2D>());
+    }
+}
+
